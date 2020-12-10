6abab6e8674fd9ff7c688da4d4f946a7980dd8e2
Josée Paquette <josee.n.paquette@hrsdc-rhdcc.gc.ca>
2020-12-10 15:42:43 -0500

    added Transactions

--- BankAccount.cs
+++ BankAccount.cs
@@ -8,6 +8,56 @@ namespace ObjectOrientedProject
 {
     public class BankAccount
     {
+        
+
+        /// <summary>
+        /// Followed this tutorial: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/intro-to-csharp/introduction-to-classes
+        /// </summary>
+        public string Number { get; }
+        public string Owner { get; set; }
+        
+        /// <summary>
+        /// Use this property to compute the balance.
+        /// It sums the values of all transactions.
+        /// The calculation 
+        /// </summary>
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            } 
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
         /// <summary>
         /// This is a data member. It is private, it can only be accessed by code inside the BankAccount class.
         /// How account numbers are generated are kept private, and shared by all the BankAccount objects.
@@ -15,28 +65,24 @@ namespace ObjectOrientedProject
         private static int accountNumberSeed = 1234567890;
 
         /// <summary>
-        /// Followed this tutorial: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/intro-to-csharp/introduction-to-classes
+        /// A constructor to assign the values. 
+        /// Used to initialize objects of the BankAccount type
         /// </summary>
-        public string Number { get; }
-        public string Owner { get; set; }
-        public decimal Balance { get; }
-
-        public void MakeDeposit(decimal amount, DateTime date, string note)
-        {
-
-        }
-
-        public void MakeWithdrawal(decimal amount, DateTime date, string note)
-        {
-
-        }
-
+        /// <param name="name"></param>
+        /// <param name="initialBalance"></param>
         public BankAccount(string name, decimal initialBalance)
         {
-            this.Owner = name;
-            this.Balance = initialBalance;
+            //this.Balance = initialBalance;
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
         }
+        /// <summary>
+        /// TransactionalDeclaration
+        /// </summary>
+        private List<Transaction> allTransactions = new List<Transaction>();
+
     }
 }
---
+++ Transaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProject
+{
+    public class Transaction
+    {
+       
+        /// <summary>
+        /// Creates a type to represent a transaction, but doens't have any responsibilities.
+        /// Includes a few properties.
+        /// </summary>
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Notes { get; set; }
+
+        /// <summary>
+        /// This is to keep an audit, history of each account transaction.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="date"></param>
+        /// <param name="note"></param>
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
